4580eadb7e300635e91e2e736a0a511ddf089770
Kammy <blahblahkams@yahoo.com>
2015-12-24 01:04:52 -0500

    complete chapter 5 exercises

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,5 +10,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path, count: 2
+
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
+
 end
