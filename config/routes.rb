3440c6f07e44f5dabd9a0e9a059ce34cde8f4796
Kammy <blahblahkams@yahoo.com>
2015-12-25 17:47:28 -0500

    add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
   root 'static_pages#home'
   get 'help' 	=> 'static_pages#help'
   get 'about' 	=> 'static_pages#about'
@@ -17,6 +13,7 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activation, only: [:edit]
   resources :password_resets, 	only: [:new, :create, :edit, :update]
+  resources :microposts, 	only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
