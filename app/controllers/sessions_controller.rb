8f899cd68be58bb2991e39f76f65dc79d656b0ed
Kammy <blahblahkams@yahoo.com>
2015-12-24 23:19:29 -0500

    Finish user index, edit, update, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user #user_url(user)     
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+    redirect_back_or @user 
+    # redirect_to @user #@user_url(@user)     
     else 
       flash.now[:danger] = 'Invalid email/password combination'
           #.now --> display flash on rendered pages, disappears on first request
